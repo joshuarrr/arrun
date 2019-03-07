67e39a5d70813f09667374c9bc457ac8dacaa37f
Joshua Richey <j@joshuar.com>
2019-03-06 16:22:34 -0800

    Init

---
+++ src/app/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <header className='app-header'>
+          <h1>WELCOME TO GAME</h1>
+        </header>
+      </div>
+    )
+  }
+}
+
+export default App
